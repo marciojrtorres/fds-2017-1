d8974071200a3cfd501f79cd3a321a87d359c996
marciojrtorres <marciojrtorres@gmail.com>
2017-05-08 20:55:47 -0300

    fds

---
+++ cobaiasqlserver2/cobaiasqlserver2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cobaiasqlserver2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // INSERT! ORM!
+            Cliente c1 = new Cliente
+            {
+                Nome = "Gustavo Prestes"
+            };
+            // equivale ao EntityManager do JPA (Java)
+            dbEntities db = new dbEntities();
+            db.Clientes.Add(c1);
+
+            Console.WriteLine(c1.Id);
+
+            db.SaveChanges();
+
+            Console.WriteLine(c1.Id);
+
+            Pedido p1 = new Pedido
+            {
+                Descricao = "Um pedido do Gustavo"
+            };
+
+            c1.Pedidos.Add(p1);
+            p1.Cliente = c1;
+
+            db.SaveChanges();
+            // LINQ
+            var c2 = db.Clientes.Where(c => c.Nome.StartsWith("a"));
+            
+            Console.WriteLine(c2.ToList());
+
+            
+        }
+    }
+}
---
+++ cobaiasqlserver2/cobaiasqlserver2/schema.sql
@@ -0,0 +1,14 @@
+﻿-- DDL ou SCHEMA
+CREATE TABLE Clientes (
+	Id INT IDENTITY,
+	Nome NVARCHAR(50),
+	PRIMARY KEY (Id)
+);
+
+CREATE TABLE Pedidos (
+	Id INT IDENTITY,
+	Descricao TEXT,
+	ClienteId INT,
+	PRIMARY KEY (Id),
+	FOREIGN KEY (ClienteId) REFERENCES Clientes (Id)
+);
